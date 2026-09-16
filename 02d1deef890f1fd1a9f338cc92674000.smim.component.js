cd71b48af96331bdebff33cb888fd8470ff385db
Duygu Gözde Tümer <gozden@gmail.com>
2026-09-16 14:42:20 +0300

    Delete 02d1deef890f1fd1a9f338cc92674000.smim.component.js

--- 02d1deef890f1fd1a9f338cc92674000.smim.component.js
+++
@@ -1,8 +0,0 @@
-sap.ui.define([
-  "sap/suite/ui/generic/template/lib/AppComponent"
-], function (AppComponent) {
-  "use strict";
-  return AppComponent.extend("zvdx.notfe.Component", {
-    metadata: { manifest: "json" }
-  });
-});
